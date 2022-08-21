150be5e52461203cd05875d8f9c15916ef25c234
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-21 23:41:08 +0100

    configure state management for login inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  return (
-    <div className="App d-flex justify-content-center align-items-center">
-      <form className='border'>
-        <div class="m-3">
-          <label for="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+    const [loginForm, setLoginFrom] = useState({
+        userName: '',
+        password: ''
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(loginForm)
+    }
+
+    return (
+        <div className="App d-flex justify-content-center align-items-center">
+            <form className='border' onSubmit={handleSubmit}>
+                <div className="m-3">
+                    <label className="form-label">Email address</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={loginForm.userName}
+                        onChange={(e) => { setLoginFrom({ ...loginForm, userName: e.target.value }) }}
+                    />
+                </div>
+                <div className="m-3">
+                    <label className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={loginForm.password}
+                        onChange={(e) => { setLoginFrom({ ...loginForm, password: e.target.value }) }}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary m-3">Submit</button>
+            </form>
         </div>
-        <div className="m-3">
-          <label for="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <button type="submit" className="btn btn-primary m-3">Submit</button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Login;
