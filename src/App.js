40ac00386a5a287f544b23d1bbd99bc6f7ba0ccf
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 00:20:54 +0100

    Add routing system for the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css'
 import Login from './pages/Login'
+import Home from './pages/Home'
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+  Navigate,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      < Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="*"
+            element={<Navigate to={true ? '/home' : "/"} />}
+          />
+          {true ?
+            <>
+              <Route path="/home" element={<Home />} />
+            </>
+            :
+            <Route path="/" element={<Login />} />
+          }
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
