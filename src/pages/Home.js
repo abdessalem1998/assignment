1e0904758a66bb8fb18daeda97f77781e23b4de9
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 10:40:30 +0100

    display data dynamically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,26 +18,38 @@ function Home() {
         <div>
             <label className="my-3">Home page</label>
             <div className="mb-5 container">
-                <input 
-                    type="text" 
-                    placeholder='search ...' 
+                <input
+                    type="text"
+                    placeholder='search ...'
                     value={find}
-                    onChange={(e) => { setFind( e.target.value ) }} 
-                    className="form-control mx-auto" 
-                    />
+                    onChange={(e) => { setFind(e.target.value) }}
+                    className="form-control mx-auto"
+                />
             </div>
             <div className='row'>
                 {
-                    users.map((user,index) => (
-                        <div className="card col-sm-3 p-2" key={index}>
-                            <ul className="list-group list-group-flush m-2">
-                                <li className="list-group-item">An item</li>
-                                <li className="list-group-item">A second item</li>
-                                <li className="list-group-item">A third item</li>
-                            </ul>
-                        </div>
-                    ))
-                    }
+                    find === ''
+                        ? <h3>search ....</h3>
+                        : users.map((user, index) => (
+                            <div className=" col-sm-3 p-2 my-2" key={index}>
+                                <ul className="card list-group list-group-flush">
+                                    <li className="list-group-item"><p><b>username</b></p><p>{user.username}</p></li>
+                                    <li className="list-group-item"><p><b>password</b></p><p>{user.password}</p></li>
+                                    <li className="list-group-item"><p><b>profile_picture_url</b></p><p>{user.profile_picture_url}</p></li>
+                                    <li className="list-group-item"><p><b>otif_points_balance</b></p><p>{user.otif_points_balance}</p></li>
+                                    <li className="list-group-item"><p><b>email</b></p><p>{user.email}</p></li>
+                                    <li className="list-group-item"><p><b>refer</b></p><p>{user.refer}</p></li>
+                                    <li className="list-group-item"><p><b>phone_num</b></p><p>{user.phone_num}</p></li>
+                                    <li className="list-group-item"><p><b>first_name</b></p><p>{user.first_name}</p></li>
+                                    <li className="list-group-item"><p><b>last_name</b></p><p>{user.last_name}</p></li>
+                                    <li className="list-group-item"><p><b>birthday</b></p><p>{user.birthday}</p></li>
+                                    <li className="list-group-item"><p><b>verified</b></p><p>{user.verified}</p></li>
+                                    <li className="list-group-item"><p><b>verification_hash</b></p><p>{user.verification_hash}</p></li>
+                                </ul>
+                            </div>
+                        ))
+
+                }
             </div>
         </div>
     );
