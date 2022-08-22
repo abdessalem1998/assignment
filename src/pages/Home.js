2d00e322cafe55556f8237cdb0d19c378d0484f0
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 01:42:54 +0100

    Consume the get Api to fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 function Home() {
+    useEffect(() => {
+        axios.get('https://otif-server-dot-otif-mx.uc.r.appspot.com/access')
+            .then((response) => {
+                console.log(response)
+            }, (error) => {
+                console.log(error);
+            });
+    }, []);
     return (
         <div>
             <label className="my-3">Home page</label>
             <div className="mb-5 container">
                 <input type="text" placeholder='search ...' className="form-control mx-auto" />
             </div>
-            <div className='row'> 
+            <div className='row'>
                 <div className="card col-sm-3 m-2">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">An item</li>
