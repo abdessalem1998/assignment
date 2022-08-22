9c7e6a43521d69b6fb365f9d510c3ae6c57a615f
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 01:39:05 +0100

    Fix Mobile vue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,18 @@ function Home() {
     return (
         <div>
             <label className="my-3">Home page</label>
-            <div className="mb-5">
-                <input type="text" placeholder='search ...' className="form-control w-25 mx-auto" />
+            <div className="mb-5 container">
+                <input type="text" placeholder='search ...' className="form-control mx-auto" />
             </div>
             <div className='row'> 
-                <div className="card col-3 mx-2">
+                <div className="card col-sm-3 m-2">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">An item</li>
                         <li className="list-group-item">A second item</li>
                         <li className="list-group-item">A third item</li>
                     </ul>
                 </div>
-                <div className="card col-3 mx-2">
+                <div className="card col-sm-3 m-2">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">An item</li>
                         <li className="list-group-item">A second item</li>
