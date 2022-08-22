af851267d849a739326222bb994c207d3ff92b5e
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 01:29:30 +0100

    Design search input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      home page
-    </div>
-  );
+    return (
+        <div>
+            <label className="my-3">Home page</label>
+            <div className="">
+                <input type="text" placeholder='search ...' className="form-control w-25 mx-auto"  />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
