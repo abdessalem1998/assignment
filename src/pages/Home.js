abb21ba346c88c1a5c2a16bd7682f6862398f540
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-08-22 10:15:09 +0100

    Map the data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 function Home() {
+    const [users, setUsers] = useState([])
     useEffect(() => {
         axios.get('https://otif-server-dot-otif-mx.uc.r.appspot.com/access')
             .then((response) => {
-                console.log(response)
+                setUsers(response.data)
             }, (error) => {
                 console.log(error);
             });
     }, []);
+    console.log(users)
     return (
         <div>
             <label className="my-3">Home page</label>
@@ -17,20 +19,17 @@ function Home() {
                 <input type="text" placeholder='search ...' className="form-control mx-auto" />
             </div>
             <div className='row'>
-                <div className="card col-sm-3 m-2">
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
-                </div>
-                <div className="card col-sm-3 m-2">
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
-                </div>
+                {
+                    users.map((user,index) => (
+                        <div className="card col-sm-3 p-2" key={index}>
+                            <ul className="list-group list-group-flush m-2">
+                                <li className="list-group-item">An item</li>
+                                <li className="list-group-item">A second item</li>
+                                <li className="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                    ))
+                    }
             </div>
         </div>
     );
